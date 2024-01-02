c2abd86aaaec4c448e2e8f9083f318297a9e23b0
ando <terbu102@gmail.com>
2024-01-02 09:18:32 +0300

    alx ecmascript 6

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+//anonymous function add
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
