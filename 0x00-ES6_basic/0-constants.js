3a6e43bb100429b1ab16e2f24baa365833b3b8c4
ando <terbu102@gmail.com>
2024-01-03 21:06:00 +0300

    basic task

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,16 +1,15 @@
-//
 export function taskFirst() {
-    const task = 'I prefer const when I can.';
+     task = 'I prefer const when I can.';
     return task;
   }
   
-// export function getLast() {
-//     return ' is okay';
-//   }
+export function getLast() {
+    return ' is okay';
+  }
   
 export function taskNext() {
     let combination = 'But sometimes let';
-    combination += ' is okay';
+    combination += getLast();
   
     return combination;
   }
