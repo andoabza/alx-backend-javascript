81e23f5029cc43ad489e0b7500262bb3b2d37c02
ando <terbu102@gmail.com>
2024-01-04 20:51:03 +0300

    eslinter

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,35 +1,50 @@
-export default class HolbertonCourse{
-    constructor(name, length, students){
-       if (typeof name !== 'string') throw Error('Name must be a string');
-       if (typeof length !== 'number') throw Error('Length must be a number');
-    
-       this._name = name;
-       this._length = length;
-       this._students = students;    
+export default class HolbertonCourse {
+    constructor(name, length, students) {
+      if (typeof name !== 'string') {
+        throw TypeError('Name must be a string');
+      }
+      if (typeof length !== 'number') {
+        throw TypeError('Length must be a number');
+      }
+      if (!Array.isArray(students) || students.some(student => typeof student !== 'string')) {
+        throw TypeError('Students must be an array of strings');
+      }
+  
+      this._name = name;
+      this._length = length;
+      this._students = students;
     }
-
-    get name(){
-        return this._name;
+  
+    get name() {
+      return this._name;
     }
-
-    get length(){
-        return this._length;
-    }
-
-
-    get students(){
-        return this._students
-    }
-
+  
     set name(newName) {
-        this._name = newName;
+      if (typeof newName !== 'string') {
+        throw TypeError('Name must be a string');
+      }
+      this._name = newName;
     }
- 
+  
+    get length() {
+      return this._length;
+    }
+  
     set length(newLength) {
-        this._length = newLength;
+      if (typeof newLength !== 'number') {
+        throw TypeError('Length must be a number');
+      }
+      this._length = newLength;
     }
- 
+  
+    get students() {
+      return this._students;
+    }
+  
     set students(newStudents) {
-        this._students = newStudents;
+      if (!Array.isArray(newStudents) || newStudents.some(student => typeof student !== 'string')) {
+        throw TypeError('Students must be an array of strings');
+      }
+      this._students = newStudents;
     }
- }
+  }
--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -3,19 +3,23 @@ export default class Currency{
     this.code = code;
     this.name = name;
   }
+  
   get code(){
       return this._code;
     }
+
   get name(){
       return this._name;
     }
+
   set code(code){
         this._code = code;
     }
+
   set name(name){
     this._name = name;
   }
-  
+
   /**
    * Creates the full string representation of this Currency.
    * @returns {String}
