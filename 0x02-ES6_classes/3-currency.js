242149eb6f4f4d9cc0e6b81de1fc31ccd51db17f
ando <terbu102@gmail.com>
2024-01-04 20:47:23 +0300

    eslinter

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,7 +1,7 @@
 export default class Currency{
   constructor(code, name){
-    this._code = code;
-    this._name = name;
+    this.code = code;
+    this.name = name;
   }
   get code(){
       return this._code;
@@ -16,6 +16,6 @@ export default class Currency{
     this._name = name;
   }
   displayFullCurrency(){
-    return `${this._name} (${this._code})`;
+    return `${this.name} (${this.code})`;
   }
 }
