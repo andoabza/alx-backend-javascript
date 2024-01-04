317513e575cda4ca63e6518816c001b55f3e068d
ando <terbu102@gmail.com>
2024-01-04 22:06:58 +0300

    price conversion

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building{
+    constructor(sqft){
+        if (this.evacuationWarningMessage === undefined) {
+            throw new TypeError('Class extending Building must override evacuationWarningMessage);
+          }
+        this._sqft = sqft;
+    }
+
+    get sqft(){
+        return this._sqft;
+    }
+
+    set sqft(newSqft){
+        this._sqft = newSqft;
+    }
+
+
+}
