5669fad6c7c5e79594ba19834479d0bd1ea7b38a
ando <terbu102@gmail.com>
2024-01-09 19:48:56 +0300

    weak map()

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,10 @@
+import WeakMap from 'weakmap';
+export const weakmap = new WeakMap();
+
+export default function queryAPI({ protocol, name }) {
+    const endpoint = `${protocol}://${name}`;
+    let called = weakmap.get(endpoint) || 0;
+    called += 1;
+    weakmap.set(endpoint, called);
+    if (called >= 5) throw Error('Endpoint load is high');    
+}
