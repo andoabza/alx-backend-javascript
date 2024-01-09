b67885ac557a8ab17184207c993685f4e9a5ef43
ando <terbu102@gmail.com>
2024-01-09 19:07:23 +0300

    str

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,10 @@
-export default function(set, startString) {
-   let result = '';
-    for (let item of set) {
-       if (item.startsWith(startString)) {
-           result += item.slice(startString.length) + '-';
-       }
-   }
-   if (startString.length === 0) {
-    return '';
+export default function cleanSet(set, startString) {
+    if (!startString || typeof startString !== 'string') return '';
+    let result = '';
+    for (const item of set) {
+      if (item.startsWith(startString)) {
+        result += item.slice(startString.length) + '-';
+      }
     }
-   return result.slice(0, -1);
-}
+    return result.slice(0, -1); // remove trailing '-'
+  }
