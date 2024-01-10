41d2fd0ded2614db6569bd3d67bae3db0a0a44ff
ando <terbu102@gmail.com>
2024-01-10 13:52:42 +0300

    directors object with optional and add values

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,12 +7,6 @@ interface Teacher {
     [key: string]: any;
  }
 
-const teacher3: Teacher = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false,
-  };
-  
-  console.log(teacher3);
+ interface Directors extends Teacher {
+    numberOfReports: number;
+}
