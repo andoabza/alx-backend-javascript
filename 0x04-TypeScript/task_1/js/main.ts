90cba70c08c110ba8d935f8d73bfb999958bd7b7
ando <terbu102@gmail.com>
2024-01-10 18:44:36 +0300

    student class implements studentclasss interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,6 +7,33 @@ interface Teacher {
     [key: string]: any;
  }
 
- interface Directors extends Teacher {
+interface Directors extends Teacher {
     numberOfReports: number;
 }
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName[0]}. ${lastName}`;
+}
+
+class StudentClass implements StudentClass{
+    firstName: string;
+    lastName: string;
+    
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(){
+        return 'Currently working';
+    }
+    displayName(){
+        return this.firstName;
+    }
+}
+
+
