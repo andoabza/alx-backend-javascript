d8b5f7fb1058f4d3f804889e0e95a098f8595d04
ando <terbu102@gmail.com>
2024-01-10 19:11:30 +0300

    task 2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,44 @@
+interface DirectorInterface {
+    workFromHome() :string;
+    getCoffeeBreak() :string;
+    workDirectorTasks() :string;
+}
+
+interface TeacherInterface {
+    workFromHome() :string;
+    getCoffeeBreak() :string;
+    workDirectorTasks() :string;
+    
+}
+
+class Director implements DirectorInterface {
+    workFromHome() {
+        return 'Working from home';
+    }
+    getCoffeeBreak() {
+        return 'Getting a coffee break';
+    }
+    workDirectorTasks() {
+        return 'Getting to director tasks';
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome() {
+        return 'Cannot work from home';
+    }
+    getCoffeeBreak() {
+        return 'Cannot have a break';
+    }
+    workDirectorTasks() {
+        return 'Getting to work';
+    }
+}
+
+function createEmployee(salary: number | string){
+    if(typeof salary === 'number' && salary < 500) {
+        return new Teacher();
+    }
+    return new Director();
+
+}
