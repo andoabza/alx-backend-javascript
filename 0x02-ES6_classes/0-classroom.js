3f80e99917b1fa9e2aa4fefa0094a474868c100d
ando <terbu102@gmail.com>
2024-02-17 09:55:23 +0300

    classes

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,13 @@
-/*classs room*/
+/**
+ * Represents a class room.
+ */
 export default class ClassRoom {
-  constructor (maxStudentsSize) {
+  /**
+   * Creates a new @see {@link ClassRoom}.
+   * @param {Number} maxStudentsSize - The maximum number of
+   * students in the class.
+   */
+  constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
 }
