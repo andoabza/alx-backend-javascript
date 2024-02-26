812b52a93fa0ba67e75c0de5a933daca90650930
ando <terbu102@gmail.com>
2024-02-26 21:34:55 +0300

    std in

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,11 +8,15 @@ const r1 = readline.createInterface({
 });
 
 console.log('Welcome to Holberton School, what is your name?');
+
 r1.question('', (name) => {
   console.log(`Your name is: ${name}`);
   r1.close();
 });
-
-r1.on('close', () => {
-  console.log('This important software is now closing');
+if (process.stdin.isTTY === undefined) {
+  r1.on('close', () => {
+    console.log('This important software is now closing');
+  
 });
+}
+module.exports = r1;
