f11b552ee132db0a8e032326a1f0d6b2fe6387fa
ando <terbu102@gmail.com>
2024-02-26 10:19:44 +0300

    std in

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,11 +5,9 @@ const readline = require('readline');
 
 const r1 = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
-
 r1.question('', (name) => {
   console.log(`Your name is: ${name}`);
   r1.close();
