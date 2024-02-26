4cc81caa72fd45387b4cfae0ebbf9c4adf50ba42
ando <terbu102@gmail.com>
2024-02-26 22:05:00 +0300

    std in

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -4,12 +4,30 @@
 const fs = require('fs');
 
 function countStudents(path) {
-  fs.readFile(path, 'utf-8', (err, data) => {
+  fs.readFile(path, 'utf-8', (err, data) => {  
     if (err) {
       throw new Error('Cannot load the database');
     }
     let arr = [data];
-    console.log('Number of students:', arr.map((element) => console.log(element.length)));
-  });
+    let a = {}
+    arr.map((element) => a = element.split('\n'));
+    console.log(`Number of students: ${a.length - 1}`);
+    let i = 0;
+    let l = 0;
+    let cs = '';
+    let swe = '';
+    for (let j = 0; j < a.length; j++) {
+      if (a[j].includes('CS')) {
+        cs = a[j].split(',')[0]+ ', ' + cs;
+        i++;
+      }
+        if (a[j].includes('SWE')) {
+            swe = a[j].split(',')[0]+ ', ' + swe;
+            l++;
+        }
+    }
+    console.log(`Number of students in CS: ${i}. List`, cs);
+    console.log(`Number of students in SWE: ${l}. List`, swe);
+});
 }
-module.exports = countStudents;
+countStudents('database.csv');
