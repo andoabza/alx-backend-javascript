e6770c62e1aa0758a868f41dbf28adb3a0d9ccc9
ando <terbu102@gmail.com>
2024-02-26 09:21:11 +0300

    console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/* 
+    a function that STDOUT argument  
+*/
+function displayMessage(arg){
+    /*
+    displayMessage: a function that print the arg
+    */
+    console.log(arg)
+}
+module.exports = displayMessage;
