fb4a6970fecc6b8aa3b0d226dbaaa51c4f6db807
ando <terbu102@gmail.com>
2024-02-27 11:49:06 +0300

    read file

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,3 +1,4 @@
+
 /*
   a function that reads databasefile
 */
@@ -7,30 +8,30 @@ function countStudents(path) {
   /*
     countStudents: a function that count students in databse
   */
-  fs.readFile(path, 'utf-8', (err, data) => {  
+  fs.readFile(path, 'utf-8', (err, data) => {
     if (err) {
       throw new Error('Cannot load the database');
     }
-    let arr = [data];
-    let a = {}
+    const arr = [data];
+    let a = {};
     arr.map((element) => a = element.split('\n'));
     console.log(`Number of students: ${a.length - 1}`);
     let i = 0;
     let l = 0;
-    let cs = '';
-    let swe = '';
+    let cs = [];
+    let swe = [];
     for (let j = 0; j < a.length; j++) {
       if (a[j].includes('CS')) {
-        cs = a[j].split(',')[0]+ ', ' + cs;
+        cs.push(a[j].split(',')[0]);
         i++;
       }
-        if (a[j].includes('SWE')) {
-            swe = a[j].split(',')[0]+ ', ' + swe;
-            l++;
-        }
+      if (a[j].includes('SWE')) {
+        swe.push(a[j].split(',')[0]);
+        l++;
+      }
     }
-  console.log(`Number of students in CS: ${i}. List`, cs);
-  console.log(`Number of students in SWE: ${l}. List`, swe);
-});
+    console.log(`Number of students in CS: ${i}. List`, cs);
+    console.log(`Number of students in SWE: ${l}. List`, swe);
+  });
 }
 module.exports = countStudents;
