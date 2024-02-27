bee9b9ee485852a3c3db72d192f4e8389e96d22f
ando <terbu102@gmail.com>
2024-02-27 22:15:26 +0300

    http

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = 'localhost';
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
+});
+app.listen(port, hostname, () => {
+  console.log('...');
+});
+module.exports = app;
