3bda8705f64589f71f33ad1ac08a7b56ede7c2b8
ando <terbu102@gmail.com>
2024-03-04 20:42:14 +0300

    calculation test

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,2 +1,8 @@
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
+/*calculateNumber: a function that calcilate numbers*/
+const calculateNumber = (a, b) => {
+  a = Math.round(a);
+  b = Math.round(b);
+  return eval(a + b);
+}
+
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,18 +2,16 @@ const assert = require('assert');
 const calculateNumber = require("./0-calcul");
 
 describe('calculateNumber', function() {
-    it('4', function() {
+    it('...', function() {
         assert.strictEqual(calculateNumber(1, 3), 4);
     })
-    it('5', function() {
+    it('...', function() {
         assert.strictEqual(calculateNumber(1.5, 3), 5);
     })
-    it('5', function() {
+    it('...', function() {
         assert.strictEqual(calculateNumber(1.5, 3.5), 6);
     })
-    it('6', function() {
+    it('...', function() {
         assert.strictEqual(calculateNumber(1.4, 3.5), 5);
     })
 });
-
-module.exports = calculateNumber;
